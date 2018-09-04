767cd14e880cb0004c62c0f7de460e46ffe84e31
muthomidenishub <33800472+muthomidenishub@users.noreply.github.com>
2018-09-04 06:05:14 +0000

    Final cashier class

--- cashier.php
+++ cashier.php
@@ -1,81 +1,83 @@
 <?php
-    // include database and object files
-     require_once 'C:\xampp\htdocs\GROUP4\config\dbconnection.php';
-     require_once 'C:\xampp\htdocs\GROUP4\Admin\cashiercreate.php';
+require_once 'C:\xampp\htdocs\GROUP4\config\dbconnection.php';
+require_once 'C:\xampp\htdocs\GROUP4\object\order.php';
+require_once 'C:\xampp\htdocs\GROUP4\Cashier\cashlay.php';
+$db = new dbConnect();
+$database = $db->connect();
+$orders = new Order($database);
+//fetch all Products
+$statement =$orders->readOrders();
+$numrow =$statement->rowCount();
+ ?>
+ <html>
 
-    // get database connection
-    $database = new dbConnect();
-    $db = $database->connect();
+ <script src='https://ajax.googleapis.com/ajax/libs/jquery/jquery/2.2.0/jquery.min.js'>
+ </script>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+ <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+ <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css" />
 
-    // pass connection to objects
-    $cashier = new Cashier($db);
-    // set page headers
-    $page_title = "Create Cashier";
-     require_once 'C:\xampp\htdocs\GROUP4\adminlay.php';
 
-     echo "<div class='right-button-margin'>";
-     echo "<a href='AllProducts.php' class='btn btn-primary custom-btn pull-right' style='margin-bottom:2%'>Read Products</a>";
- echo "</div>";
+
+
+
+
+<head>
+  <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
+  <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+  </head>
+
+</head>
+<body>
+<br/><br/>
+<div class="container">
+  <h3>Search Order</h3>
+</br>
+<div class="table-responsive">
+  <table id="order_table" class="table table-striped table-bordered">
+    <thead>
+      <tr>
+      <td>ID</td>
+      <td>User Name</td>
+      <td>Created</td>
+      <td>Action</td>
+    </tr>
+    </thead>
+    <?php
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+  extract($row);
+  echo"<tbody>";
+
+  echo "<tr>";
+      echo "<td>{$id}</td>";
+      echo "<td>{$user_id}</td>";
+      echo "<td>{$created}</td>";
+      echo "<td>";
+        // read product button
+
+// edit product button
+echo "<a href='execute.php?id={$id}' class='btn btn-info left-margin'>";
+echo "<span class='glyphicon glyphicon-edit'></span> Exeecute";
+echo "</a>";
+echo "</td>";
+
+echo "</tr>";
+  // code...
+}
 
     ?>
-    <!-- PHP post code will be here -->
-    <?php
-// if the form was submitted
-if($_POST){
-
-    // set product property values
-    $cashier->username = $_POST['name'];
-    $cashier->email = $_POST['email'];
-    $cashier->password=$_POST['password'];
+  </table>
+  <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+  <script>
+ $(document).ready(function(){
+   $('#order_table').DataTable();
+ });
 
 
-    // create the product
+  </script>
 
-    if($cashier->createCashier()){
-
-        echo "<div class=container>
-        <div class='alert alert-success alert-dismissable'>
-          <a href='#' class='close' data-dismiss='alert' aria-label='close'>×</a>
-          <strong>Cashier Created successfuly succesfuly</strong>
-        </div>
-      </div>";
-          // try to upload the submitted file
-// uploadPhoto() method will return an error message, if any.
-    }
-    }
-
- ?>
- <!-- HTML form for creating a product -->
- <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
-    <div class="container-fluid">
-    <div class="row grid">
-    <div class="col-md-9">
-    <table class='table table-hover table-responsive-md table-bordered' width=10%>
-
-
-        <tr>
-            <td>Cashier Name</td>
-            <td><input type='text' name='name' placeholder="enter name"class='form-control' width='50%' /></td>
-        </tr>
-            <tr>
-            <td>Cashier Email</td>
-            <td><input type='email' name='email' placeholder="Enter email address" class='form-control' /></td>
-        </tr>
-        <tr>
-            <td>Cashier Password</td>
-            <td><input type='password' name='password' placeholder="Enter password" class='form-control' /></td>
-        </tr>
-        <tr>
-        <td><input type='hidden' name='rights' value='1' class='form-control' width='50%' /></td>
-        <tr>
-        <td>Action</td>
-        <td>
-                    <button type="submit" class="btn btn-primary" pull-left>Create</button>
-                </td>
-        </tr>
-
-    </table>
-    </div>
-    </div>
-    </div>
-</form>
+</body>
+ </html>
+ <!--activate data dataTables-->
